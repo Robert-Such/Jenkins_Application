2fd36ac8c709caa83feb2daa4d07e86e03cfd4ba
RS-TEST <rs-test@test.localdomain>
2024-12-20 12:37:26 -0600

    initial commit for application

---
+++ src/main/java/org/example/application/Main.java
@@ -0,0 +1,9 @@
+package org.example.application;
+
+import org.example.library.Utility;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utility.greet("World"));
+    }
+}
