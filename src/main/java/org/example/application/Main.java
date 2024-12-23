39c54b5e00854ba50f58a615fce49808d2168c99
RS-TEST <rs-test@test.localdomain>
2024-12-23 15:30:32 -0600

    changed to getenv

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -4,7 +4,7 @@ import org.example.library.Utility;
 
 public class Main {
     public static void main(String[] args) {
-        String name = System.getProperty("TEST_NAME");
+        String name = System.getenv("TEST_NAME");
         System.out.println(Utility.greet(name));
     }
 }
